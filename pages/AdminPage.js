a75985e3c5c7987e35ed1a8fd19c68c783dd6e17
Stoycho Enchev (EXT-Nokia) <stoycho.enchev.ext@nokia.com>
2024-11-12 17:10:30 +0200

    HomePage and new tests for the homepage visibility of the buttons

--- pages/AdminPage.js
+++ pages/AdminPage.js
@@ -3,8 +3,8 @@ exports.AdminPage = class AdminPage {
     constructor(page) {
 
         this.page = page
-        this.adminBtn = page.getByRole('link', { name: 'Admin' });
-        this.addBtn = page.getByRole('button', { name: ' Add' });
+        this.adminButton = page.getByRole('link', { name: 'Admin' });
+        this.addButton = page.getByRole('button', { name: ' Add' });
         this.formI = page.locator('form i').first();
         this.formIOption = page.getByRole('option', { name: 'Admin' });
         this.typeForHints = page.getByPlaceholder('Type for hints...');
@@ -13,16 +13,16 @@ exports.AdminPage = class AdminPage {
         this.formEnabled = page.getByRole('option', { name: 'Enabled' });
         this.usernameBox = page.getByRole('textbox').nth(2);
         this.passwordBox = page.getByRole('textbox').nth(3);
-        this.passwordBoxRepat = page.getByRole('textbox').nth(4)
-        this.saveBtn = page.getByRole('button', { name: 'Save' })
+        this.passwordBoxRepat = page.getByRole('textbox').nth(4);
+        this.saveButton = page.getByRole('button', { name: 'Save' })
 
     }
 
     async navigateToAdminPageAndAddUser() {
         const uniqueUsername = `user_${Date.now()}`; // Creates a unique username for each test run
 
-        await this.adminBtn.click();
-        await this.addBtn.click();
+        await this.adminButton.click();
+        await this.addButton.click();
         await this.formI.click();
         await this.formIOption.click();
         await this.typeForHints.fill('A');
@@ -33,11 +33,12 @@ exports.AdminPage = class AdminPage {
         await this.usernameBox.fill(uniqueUsername);
         await this.passwordBox.fill('parola123');
         await this.passwordBoxRepat.fill('parola123');
-        await this.saveBtn.click();
-        
-        
+        await this.saveButton.click();
+
+
     }
-    
+
+
 
 
 
--- tests/test.spec.js
+++ tests/test.spec.js
@@ -1,23 +1,79 @@
 const { test, expect } = require('@playwright/test')
 const { LoginPage } = require ('../pages/LoginPage');
 const { AdminPage } = require('../pages/AdminPage');
-
+const { HomePage } = require('../pages/HomePage');
 
 test.beforeEach(async ({ page }) => {
-    const Login = new LoginPage(page)
-    await Login.navigateToPage();
-    await Login.enterUsername();
-    await Login.enterPassword();
-    await Login.clickLoginButton();
+    const login = new LoginPage(page)
+    await login.login();
     await expect(page).toHaveTitle('OrangeHRM')
 })
 
+test('Verify that the admin button is visible', async ({ page }) => {
+    const admin = new AdminPage(page)
+    await expect(admin.adminButton, "Check the visibility of the admin button").toBeVisible();
+})
+
+test('Verify that the pim button is visible', async ({ page }) => {
+    const home = new HomePage(page)
+    await expect(home.pimButton, "Check the visibility of the pim button").toBeVisible();
+})
+
+test('Verify that the leave button is visible', async ({ page }) => {
+    const home = new HomePage(page)
+    await expect(home.leaveButton, "Check the visibility of the leave button").toBeVisible();
+})
+
+test('Verify that the time button is visible', async ({ page }) => {
+    const home = new HomePage(page)
+    await expect(home.timeButton, "Check the visibility of the time button").toBeVisible();
+})
+
+test('Verify that the recruitment button is visible', async ({ page }) => {
+    const home = new HomePage(page)
+    await expect(home.recruitmentButton, "Check the visibility of the recruitment button").toBeVisible();
+})
+
+test('Verify that the myInfo button is visible', async ({ page }) => {
+    const home = new HomePage(page)
+    await expect(home.myInfoButton, "Check the visibility of the myInfo button").toBeVisible();
+})
+
+test('Verify that the performance button is visible', async ({ page }) => {
+    const home = new HomePage(page)
+    await expect(home.performanceButton, "Check the visibility of the performance button").toBeVisible();
+})
+
+test('Verify that the dashboard button is visible', async ({ page }) => {
+    const home = new HomePage(page)
+    await expect(home.dashboardButton, "Check the visibility of the dashBoard button").toBeVisible();
+})
+
+test('Verify that the directory button is visible', async ({ page }) => {
+    const home = new HomePage(page)
+    await expect(home.directoryButton, "Check the visibility of the directory button").toBeVisible();
+})
+
+test('Verify that the maintenance button is visible', async ({ page }) => {
+    const home = new HomePage(page)
+    await expect(home.maintenanceButton, "Check the visibility of the maintenance button").toBeVisible();
+})
+
+test('Verify that the claim button is visible', async ({ page }) => {
+    const home = new HomePage(page)
+    await expect(home.claimButton, "Check the visibility of the claim button").toBeVisible();
+})
+
+test('Verify that the buzz button is visible', async ({ page }) => {
+    const home = new HomePage(page)
+    await expect(home.buzzButton, "Check the visibility of the buzz button").toBeVisible();
+})
 
 test('Nav to Admin Page and Create New User', async ({ page }) => {
-    
-    const Admin = new AdminPage(page)
-    await Admin.navigateToAdminPageAndAddUser();
+    const admin = new AdminPage(page)
+    await admin.navigateToAdminPageAndAddUser();
     const successMessage = page.getByText('Success', { exact: true }); // Example success message locator
     await expect(successMessage).toBeVisible({ timeout: 100000});
-
 })
+
+
