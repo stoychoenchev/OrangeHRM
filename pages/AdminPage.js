e10ac97db0741cffdd22a634cce412216fba7491
Stoycho Enchev (EXT-Nokia) <stoycho.enchev.ext@nokia.com>
2024-11-18 21:58:22 +0200

    Recruit Page included, new tests included, Atomic Way of testing

--- pages/AdminPage.js
+++ pages/AdminPage.js
@@ -1,10 +1,8 @@
 exports.AdminPage = class AdminPage {
-
     constructor(page) {
-
         this.page = page
-        this.adminBtn = page.getByRole('link', { name: 'Admin' });
-        this.addBtn = page.getByRole('button', { name: ' Add' });
+        this.adminButton = page.getByRole('link', { name: 'Admin' });
+        this.addButton = page.getByRole('button', { name: ' Add' });
         this.formI = page.locator('form i').first();
         this.formIOption = page.getByRole('option', { name: 'Admin' });
         this.typeForHints = page.getByPlaceholder('Type for hints...');
@@ -13,16 +11,17 @@ exports.AdminPage = class AdminPage {
         this.formEnabled = page.getByRole('option', { name: 'Enabled' });
         this.usernameBox = page.getByRole('textbox').nth(2);
         this.passwordBox = page.getByRole('textbox').nth(3);
-        this.passwordBoxRepat = page.getByRole('textbox').nth(4)
-        this.saveBtn = page.getByRole('button', { name: 'Save' })
-
+        this.passwordBoxRepat = page.getByRole('textbox').nth(4);
+        this.saveButton = page.getByRole('button', { name: 'Save' });
+        this.adminPageText = page.getByRole('heading', { name: '/ User Management' });
     }
-
-    async navigateToAdminPageAndAddUser() {
+    async navigateToAdminPage() {
+        await this.page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/admin/viewSystemUsers');
+        await this.page.waitForLoadState('load');
+    }
+    async adminPageAddUser() {
         const uniqueUsername = `user_${Date.now()}`; // Creates a unique username for each test run
-
-        await this.adminBtn.click();
-        await this.addBtn.click();
+        await this.addButton.click();
         await this.formI.click();
         await this.formIOption.click();
         await this.typeForHints.fill('A');
@@ -33,12 +32,6 @@ exports.AdminPage = class AdminPage {
         await this.usernameBox.fill(uniqueUsername);
         await this.passwordBox.fill('parola123');
         await this.passwordBoxRepat.fill('parola123');
-        await this.saveBtn.click();
-        
-        
+        await this.saveButton.click();
     }
-    
-
-
-
 }
---
+++ pages/RecruitPage.js
@@ -0,0 +1,29 @@
+exports.RecruitPage = class RecruitPage {
+    constructor(page) {
+        this.page = page
+        this.recruitmentButton = page.getByRole('link', { name: 'Recruitment' });
+        this.addInRecruitmentButton = page.getByRole('button', { name: ' Add' });
+        this.firstNameField = page.getByPlaceholder('First Name');
+        this.middleNameField = page.getByPlaceholder('Middle Name');
+        this.lastNameField = page.getByPlaceholder('Last Name');
+        this.vacancyDropDown = page.locator('form i').first();
+        this.seniorQaOption = page.getByText('Senior QA Lead');
+        this.emailField = page.getByPlaceholder('Type here').first()
+        this.saveButton = page.getByRole('button', { name: 'Save' });
+    }
+    async navigateToRecruitmentPage() {
+        await this.page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/recruitment/viewCandidates');
+        await this.page.waitForLoadState('load');
+    }
+    async addRecruitment(){
+        await this.navigateToRecruitmentPage();
+        await this.addInRecruitmentButton.click();
+        await this.firstNameField.fill('Stoycho');
+        await this.middleNameField.fill('Tarkanaev');
+        await this.lastNameField.fill('Tainov');
+        await this.vacancyDropDown.click();
+        await this.seniorQaOption.click();
+        await this.emailField.fill('abekvomepitash@abv.bg');
+        await this.saveButton.click();
+    }
+}
